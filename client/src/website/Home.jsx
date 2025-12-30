8fff7ffd5400cecb5528bf985ebff5019d4d28b8
hundal44 <147229206+KuldeepEdunova@users.noreply.github.com>
2025-12-31 02:22:40 +0530

    Update Home.jsx

--- client/src/website/Home.jsx
+++ client/src/website/Home.jsx
@@ -38,12 +38,13 @@ export default function Home() {
           </p>
 
           <div className="mt-10 flex justify-center gap-6">
-            <a
+            <Link className="hover:text-teal-600" to="/shop"> Shop Now</Link>
+            {/* <a
               href="/shop"
               className="bg-white text-teal-700 px-10 py-4 rounded-full font-semibold shadow-xl hover:scale-105 transition"
             >
               Shop Now
-            </a>
+            </a> */}
             <a
               href="/about"
               className="border border-white/70 px-10 py-4 rounded-full font-semibold hover:bg-white/10 transition"
