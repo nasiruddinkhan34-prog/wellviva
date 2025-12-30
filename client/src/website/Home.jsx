92ed8b7687e45cadf7ee7d0704a01fffbe04eae7
hundal44 <147229206+KuldeepEdunova@users.noreply.github.com>
2025-12-31 03:31:09 +0530

    Update Home.jsx

--- client/src/website/Home.jsx
+++ client/src/website/Home.jsx
@@ -80,13 +80,13 @@ export default function Home() {
 
   {/* Content */}
   <div className="relative max-w-7xl mx-auto px-6 py-32 text-center text-white">
-    {/* <h1 className="text-4xl md:text-6xl font-extrabold leading-tight">
+    <h1 className="text-4xl md:text-6xl font-extrabold leading-tight">
       {slide.title}
     </h1>
 
     <p className="mt-5 text-lg text-white/90 max-w-2xl mx-auto">
       {slide.subtitle}
-    </p> */}
+    </p>
 
     <div className="mt-15 flex justify-center gap-6">
       <Link
