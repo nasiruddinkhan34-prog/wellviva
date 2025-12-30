194bbc6f5e4340e563dec4eebb25c534f617aeb3
hundal44 <147229206+KuldeepEdunova@users.noreply.github.com>
2025-12-31 03:44:02 +0530

    Update Login.jsx

--- client/src/auth/Login.jsx
+++ client/src/auth/Login.jsx
@@ -22,7 +22,7 @@ export default function Login() {
     setLoading(true);
 
     try {
-      const res = await fetch("https://wellviva-4u9a.vercel.app/api/auth/login", {
+      const res = await fetch("https://wellviva-api.onrender.com/api/auth/login", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify(form),
