9dbd36eb7370e3b28856a2e858aab90552ed0962
hundal44 <147229206+KuldeepEdunova@users.noreply.github.com>
2025-12-31 03:15:01 +0530

    Update Home.jsx

--- client/src/website/Home.jsx
+++ client/src/website/Home.jsx
@@ -67,12 +67,12 @@ export default function Home() {
   return (
     <>
       {/* ================= HERO SLIDER ================= */}
-     <section className="relative h-[320px] md:h-[450px] bg-black overflow-hidden">
+     <section className="relative h-[320px] md:h-[550px] bg-black overflow-hidden">
   {/* Background Image */}
   <img
     src={slide.image}
     alt={slide.title}
-    className="absolute inset-0 w-full h-full object-contain md:object-cover object-center transition-opacity duration-700"
+    className="absolute inset-0 object-contain md:object-cover object-center transition-opacity duration-700"
   />
 
   {/* Dark Overlay */}
