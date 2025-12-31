bc4d80579d9a3f2ac598a932fa754c5e26b0de10
hundal44 <147229206+KuldeepEdunova@users.noreply.github.com>
2025-12-31 05:40:20 +0530

    file updted

--- client/src/admin/AdminLayout.jsx
+++ client/src/admin/AdminLayout.jsx
@@ -1,15 +1,21 @@
-import AdminSidebar from "./AdminSidebar";
-import AdminHeader from "./AdminHeader";
 import { Outlet } from "react-router-dom";
+import AdminHeader from "./AdminHeader";
+import AdminSidebar from "./AdminSidebar";
+import AdminRoutes from "./AdminRoute";
 
 export default function AdminLayout() {
   return (
     <div className="flex min-h-screen bg-gray-100">
+      {/* Sidebar */}
       <AdminSidebar />
-      <div className="flex-1">
+
+      {/* Main Content */}
+      <div className="flex-1 flex flex-col">
         <AdminHeader />
+
         <main className="p-6">
           <Outlet />
+          <AdminRoutes />
         </main>
       </div>
     </div>
---
+++ client/src/admin/AdminRoute.jsx
@@ -0,0 +1,15 @@
+import { Routes, Route } from "react-router-dom";
+import Dashboard from "./pages/Dashboard";
+// later:
+// import Products from "./pages/Products";
+// import Orders from "./pages/Orders";
+
+export default function AdminRoutes() {
+  return (
+    <Routes>
+      <Route path="dashboard" element={<Dashboard />} />
+      {/* <Route path="products" element={<Products />} /> */}
+      {/* <Route path="orders" element={<Orders />} /> */}
+    </Routes>
+  );
+}
